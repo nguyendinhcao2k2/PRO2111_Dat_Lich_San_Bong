5af0948ccb4fcf3b575cdcea9d77f70fbae8d8f7
caodinh <caococ2002@gmail.com>
2023-09-08 09:18:19 +0700

    first commit

---
+++ src/main/java/com/example/pro2111_dat_lich_san_bong/Pro2111DatLichSanBongApplication.java
@@ -0,0 +1,13 @@
+package com.example.pro2111_dat_lich_san_bong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pro2111DatLichSanBongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pro2111DatLichSanBongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pro2111_dat_lich_san_bong/Pro2111DatLichSanBongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pro2111_dat_lich_san_bong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pro2111DatLichSanBongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
