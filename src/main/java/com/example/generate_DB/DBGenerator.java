aa5b749e867d848872f6232bb838d881d28a10c6
NGUYEN-QUOC-TUAN <fakerphong@gmail.com>
2023-09-17 23:17:03 +0700

    UDATE GENNERATOR

--- src/main/java/com/example/generate_DB/DBGenerator.java
+++ src/main/java/com/example/generate_DB/DBGenerator.java
@@ -3,9 +3,16 @@ package com.example.generate_DB;
 import com.example.pro2111_dat_lich_san_bong.entity.Account;
 import com.example.pro2111_dat_lich_san_bong.entity.Ca;
 import com.example.pro2111_dat_lich_san_bong.entity.ChucVu;
+import com.example.pro2111_dat_lich_san_bong.entity.DichVu;
+import com.example.pro2111_dat_lich_san_bong.entity.DoThue;
+import com.example.pro2111_dat_lich_san_bong.entity.HoaDon;
 import com.example.pro2111_dat_lich_san_bong.entity.LoaiSan;
+import com.example.pro2111_dat_lich_san_bong.entity.NuocUong;
+import com.example.pro2111_dat_lich_san_bong.entity.PhuPhi;
+import com.example.pro2111_dat_lich_san_bong.entity.PhuPhiHoaDon;
 import com.example.pro2111_dat_lich_san_bong.entity.SanBong;
 import com.example.pro2111_dat_lich_san_bong.entity.SanCa;
+import com.example.pro2111_dat_lich_san_bong.entity.ThanhToan;
 import com.example.pro2111_dat_lich_san_bong.entity.ViTien;
 import com.example.pro2111_dat_lich_san_bong.repository.AccountRepository;
 import com.example.pro2111_dat_lich_san_bong.repository.BienDongViRepository;
@@ -210,8 +217,8 @@ public class DBGenerator implements CommandLineRunner {
         ca5.setThoiGianBatDau(Time.valueOf("12:00:00"));
         ca5.setThoiGianBatDau(Time.valueOf("13:30:00"));
         ca5.setTrangThai(0);
-        ca5.setId(caRepository.save(ca5).getId());  
-        
+        ca5.setId(caRepository.save(ca5).getId());
+
         Ca ca6 = new Ca();
         ca6.setTenCa("Ca 6");
         ca6.setGiaCa(150000.0);
@@ -347,7 +354,7 @@ public class DBGenerator implements CommandLineRunner {
         san1Ca1.setIdCa(ca1.getId());
         san1Ca1.setTrangThai(0); //0 trống
         san1Ca1.setThoiGianTao(Timestamp.valueOf(format.format(dateNow)));
-        san1Ca1.setGia(sanBong1.getGiaSan()+ca1.getGiaCa());
+        san1Ca1.setGia(sanBong1.getGiaSan() + ca1.getGiaCa());
         san1Ca1.setId(sanCaRepository.save(san1Ca1).getId());
 
         SanCa san1Ca2 = new SanCa();
@@ -355,7 +362,7 @@ public class DBGenerator implements CommandLineRunner {
         san1Ca2.setIdCa(ca2.getId());
         san1Ca2.setTrangThai(0); //0 trống
         san1Ca2.setThoiGianTao(Timestamp.valueOf(format.format(dateNow)));
-        san1Ca2.setGia(sanBong1.getGiaSan()+ca2.getGiaCa());
+        san1Ca2.setGia(sanBong1.getGiaSan() + ca2.getGiaCa());
         san1Ca2.setId(sanCaRepository.save(san1Ca2).getId());
 
 
@@ -364,7 +371,7 @@ public class DBGenerator implements CommandLineRunner {
         san2Ca1.setIdCa(ca1.getId());
         san2Ca1.setTrangThai(0); //0 trống
         san2Ca1.setThoiGianTao(Timestamp.valueOf(format.format(dateNow)));
-        san2Ca1.setGia(sanBong2.getGiaSan()+ca1.getGiaCa());
+        san2Ca1.setGia(sanBong2.getGiaSan() + ca1.getGiaCa());
         san2Ca1.setId(sanCaRepository.save(san2Ca1).getId());
 
 
@@ -373,13 +380,182 @@ public class DBGenerator implements CommandLineRunner {
         san2Ca2.setIdCa(ca2.getId());
         san2Ca2.setTrangThai(0); //0 trống
         san2Ca2.setThoiGianTao(Timestamp.valueOf(format.format(dateNow)));
-        san2Ca2.setGia(sanBong2.getGiaSan()+ca2.getGiaCa());
+        san2Ca2.setGia(sanBong2.getGiaSan() + ca2.getGiaCa());
         san2Ca2.setId(sanCaRepository.save(san2Ca2).getId());
 
 
         //end san ca
 
+        ThanhToan thanhToan1 = new ThanhToan();
+        thanhToan1.setLoaiHinhThanhToan("TIỀN MẶT");
+        thanhToan1.setTrangThai(0); // 0: ĐÃ THANH TOÁN
+        thanhToan1.setId(thanhToanRepository.save(thanhToan1).getId());
 
+        ThanhToan thanhToan2 = new ThanhToan();
+        thanhToan2.setLoaiHinhThanhToan("VÍ TIỀN");
+        thanhToan2.setTrangThai(0); // 0: ĐÃ THANH TOÁN
+        thanhToan2.setId(thanhToanRepository.save(thanhToan2).getId());
+
+        ThanhToan thanhToan3 = new ThanhToan();
+        thanhToan3.setLoaiHinhThanhToan("CHỜ THANH TOÁN");
+        thanhToan3.setTrangThai(1);
+        thanhToan3.setId(thanhToanRepository.save(thanhToan3).getId());
+
+        ThanhToan thanhToan4 = new ThanhToan();
+        thanhToan4.setLoaiHinhThanhToan("VÍ TIỀN");
+        thanhToan4.setTrangThai(0); // 0: ĐÃ THANH TOÁN
+        thanhToan4.setId(thanhToanRepository.save(thanhToan4).getId());
+
+        ThanhToan thanhToan5 = new ThanhToan();
+        thanhToan5.setLoaiHinhThanhToan("CHỜ THÀNH TOÁN");
+        thanhToan5.setTrangThai(1); // 1: CHỜ THANH TOÁN
+        thanhToan5.setId(thanhToanRepository.save(thanhToan5).getId());
+
+        //END THANH TOAN
+
+        PhuPhi phuPhi1 = new PhuPhi();
+        phuPhi1.setMaPhuPhi("PP1");
+        phuPhi1.setTenPhuPhi("HỎNG BÓNG");
+        phuPhi1.setGiaPhuPhi(20000d);
+        phuPhi1.setTrangThai(0);//0: ĐÃ TRUY THU PHỤ PHÍ
+        phuPhi1.setId(phuPhiRepository.save(phuPhi1).getId());
+
+        PhuPhi phuPhi2 = new PhuPhi();
+        phuPhi2.setMaPhuPhi("PP2");
+        phuPhi2.setTenPhuPhi("HỎNG ĐỒ THUÊ");
+        phuPhi2.setGiaPhuPhi(20000d);
+        phuPhi2.setTrangThai(0);
+        phuPhi2.setId(phuPhiRepository.save(phuPhi2).getId());
+
+        PhuPhi phuPhi3 = new PhuPhi();
+        phuPhi3.setMaPhuPhi("PP3");
+        phuPhi3.setTenPhuPhi("QUÁ GIỜ");
+        phuPhi3.setGiaPhuPhi(20000d);
+        phuPhi3.setTrangThai(0);
+        phuPhi3.setId(phuPhiRepository.save(phuPhi3).getId());
+
+        //END PHỤ PHÍ
+
+        NuocUong nuocUong1 = new NuocUong();
+        nuocUong1.setTenNuocUong("STING ĐỎ");
+        nuocUong1.setSoLuong(200);
+        nuocUong1.setDonGia(8000d);
+        nuocUong1.setTrangThai(0); // 0: ĐANG BÁN 1: TẠM DỪNG
+        nuocUong1.setId(nuocUongRepository.save(nuocUong1).getId());
+
+        NuocUong nuocUong2 = new NuocUong();
+        nuocUong2.setTenNuocUong("STING VÀNG");
+        nuocUong2.setSoLuong(200);
+        nuocUong2.setDonGia(8000d);
+        nuocUong2.setTrangThai(0); // 0: ĐANG BÁN 1: TẠM DỪNG
+        nuocUong2.setId(nuocUongRepository.save(nuocUong2).getId());
+
+        NuocUong nuocUong3 = new NuocUong();
+        nuocUong3.setTenNuocUong("7 UP");
+        nuocUong3.setSoLuong(300);
+        nuocUong3.setDonGia(8000d);
+        nuocUong3.setTrangThai(0); // 0: ĐANG BÁN 1: TẠM DỪNG
+        nuocUong3.setId(nuocUongRepository.save(nuocUong3).getId());
+
+        NuocUong nuocUong4 = new NuocUong();
+        nuocUong4.setTenNuocUong("MIRIDA");
+        nuocUong4.setSoLuong(100);
+        nuocUong4.setDonGia(10000d);
+        nuocUong4.setTrangThai(0); // 0: ĐANG BÁN 1: TẠM DỪNG
+        nuocUong4.setId(nuocUongRepository.save(nuocUong4).getId());
+
+        NuocUong nuocUong5 = new NuocUong();
+        nuocUong5.setTenNuocUong("NƯỚC KHOÁNG");
+        nuocUong5.setSoLuong(500);
+        nuocUong5.setDonGia(5000d);
+        nuocUong5.setTrangThai(0); // 0: ĐANG BÁN 1: TẠM DỪNG
+        nuocUong5.setId(nuocUongRepository.save(nuocUong5).getId());
+
+        // END NƯỚC UỐNG
+
+        DoThue doThue1 = new DoThue();
+        doThue1.setTenDoThue("ÁO ĐỘI BÓNG");
+        doThue1.setSoLuong(200);
+        doThue1.setDonGia(20000d);
+        doThue1.setTrangThai(0); //0:CÒN ĐỒ THUÊ -- 1: HẾT ĐỒ THUÊ
+
+        DoThue doThue2 = new DoThue();
+        doThue2.setTenDoThue("BÓNG ĐÁ");
+        doThue2.setSoLuong(200);
+        doThue2.setDonGia(30000d);
+        doThue2.setTrangThai(0); //0:CÒN ĐỒ THUÊ -- 1: HẾT ĐỒ THUÊ
+
+        DoThue doThue3 = new DoThue();
+        doThue3.setTenDoThue("GIÀY ĐÁ BÓNG");
+        doThue3.setSoLuong(200);
+        doThue3.setDonGia(50000d);
+        doThue3.setTrangThai(0); //0:CÒN ĐỒ THUÊ -- 1: HẾT ĐỒ THUÊ
+
+        DoThue doThue4 = new DoThue();
+        doThue4.setTenDoThue("BẢNG THỐNG KÊ");
+        doThue4.setSoLuong(200);
+        doThue4.setDonGia(10000d);
+        doThue4.setTrangThai(0); //0:CÒN ĐỒ THUÊ -- 1: HẾT ĐỒ THUÊ
+
+        //END ĐỒ THUÊ
+
+        DichVu dichVu1 = new DichVu();
+        dichVu1.setIdDoThue(doThue1.getId());
+        dichVu1.setIdNuocUong(nuocUong1.getId());
+        dichVu1.setSoLuongNuocUong(5);
+        dichVu1.setSoLuongDoThue(5);
+        dichVu1.setDonGia((doThue1.getDonGia() * dichVu1.getSoLuongDoThue()) + (nuocUong1.getDonGia() * dichVu1.getSoLuongNuocUong()));
+        dichVu1.setTrangThai(0); //1: DỊCH VỤ SẴN SÀNG
+        dichVu1.setId(dichVuRepository.save(dichVu1).getId());
+
+        DichVu dichVu2 = new DichVu();
+        dichVu2.setIdDoThue(doThue2.getId());
+        dichVu2.setIdNuocUong(nuocUong2.getId());
+        dichVu2.setSoLuongNuocUong(1);
+        dichVu2.setSoLuongDoThue(3);
+        dichVu2.setDonGia((doThue2.getDonGia() * dichVu2.getSoLuongDoThue()) + (nuocUong2.getDonGia() * dichVu2.getSoLuongNuocUong()));
+        dichVu2.setTrangThai(0); //1: DỊCH VỤ SẴN SÀNG
+        dichVu2.setId(dichVuRepository.save(dichVu2).getId());
+
+        DichVu dichVu3 = new DichVu();
+        dichVu3.setIdDoThue(doThue3.getId());
+        dichVu3.setIdNuocUong(nuocUong3.getId());
+        dichVu3.setSoLuongNuocUong(15);
+        dichVu3.setSoLuongDoThue(9);
+        dichVu3.setDonGia((doThue3.getDonGia() * dichVu3.getSoLuongDoThue()) + (nuocUong3.getDonGia() * dichVu3.getSoLuongNuocUong()));
+        dichVu3.setTrangThai(0); //1: DỊCH VỤ SẴN SÀNG
+        dichVu3.setId(dichVuRepository.save(dichVu3).getId());
+
+        DichVu dichVu4 = new DichVu();
+        dichVu4.setIdDoThue(doThue4.getId());
+        dichVu4.setIdNuocUong(nuocUong4.getId());
+        dichVu4.setSoLuongNuocUong(3);
+        dichVu4.setSoLuongDoThue(2);
+        dichVu4.setDonGia((doThue4.getDonGia() * dichVu4.getSoLuongDoThue()) + (nuocUong4.getDonGia() * dichVu4.getSoLuongNuocUong()));
+        dichVu4.setTrangThai(0); //1: DỊCH VỤ SẴN SÀNG
+        dichVu4.setId(dichVuRepository.save(dichVu4).getId());
+
+        // END DỊCH VỤ
+
+        PhuPhiHoaDon phuPhiHoaDon1 = new PhuPhiHoaDon();
+        phuPhiHoaDon1.setIdPhuPhi(phuPhi1.getId());
+        phuPhiHoaDon1.setThoiGianTao(new Timestamp(System.currentTimeMillis()));
+        phuPhiHoaDon1.setTrangThai(1); // PHỤ PHÍ HÓA ĐƠN SẴN SÀNG
+        phuPhiHoaDon1.setId(phuPhiHoaDonRepository.save(phuPhiHoaDon1).getId());
+
+        PhuPhiHoaDon phuPhiHoaDon2 = new PhuPhiHoaDon();
+        phuPhiHoaDon2.setIdPhuPhi(phuPhi2.getId());
+        phuPhiHoaDon2.setThoiGianTao(new Timestamp(System.currentTimeMillis()));
+        phuPhiHoaDon2.setTrangThai(1); // PHỤ PHÍ HÓA ĐƠN SẴN SÀNG
+        phuPhiHoaDon2.setId(phuPhiHoaDonRepository.save(phuPhiHoaDon2).getId());
+
+        PhuPhiHoaDon phuPhiHoaDon3 = new PhuPhiHoaDon();
+        phuPhiHoaDon3.setIdPhuPhi(phuPhi3.getId());
+        phuPhiHoaDon3.setThoiGianTao(new Timestamp(System.currentTimeMillis()));
+        phuPhiHoaDon3.setTrangThai(1); // PHỤ PHÍ HÓA ĐƠN SẴN SÀNG
+        phuPhiHoaDon3.setId(phuPhiHoaDonRepository.save(phuPhiHoaDon3).getId());
+
+        // END PHỤ PHÍ HÓA ĐƠN
     }
 
     public static void main(String[] args) {
