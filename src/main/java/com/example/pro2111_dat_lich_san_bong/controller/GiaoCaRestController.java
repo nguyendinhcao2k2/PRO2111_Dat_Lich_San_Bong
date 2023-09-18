a6cc9c5f917e125af69378b30943f626f034e52f
MSI\Admin <dangvansy412003@gmail.com>
2023-09-18 22:14:32 +0700

    no message

--- src/main/java/com/example/pro2111_dat_lich_san_bong/controller/GiaoCaRestController.java
+++ src/main/java/com/example/pro2111_dat_lich_san_bong/controller/GiaoCaRestController.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin("*")
 @RequestMapping("/api/giao-ca")
 public class GiaoCaRestController {
 
