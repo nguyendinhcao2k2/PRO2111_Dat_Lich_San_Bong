b88ec91a93279f635bbce7ff6da6abfe883e0525
MSI\Admin <101080058+sydvph19885@users.noreply.github.com>
2023-11-19 21:44:30 +0700

    update doi lich

--- src/main/java/com/example/pro2111_dat_lich_san_bong/core/user/service/impl/SanCaUserServiceImpl.java
+++ src/main/java/com/example/pro2111_dat_lich_san_bong/core/user/service/impl/SanCaUserServiceImpl.java
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.HashMap;
