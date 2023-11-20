f263f917637248331f66df8c57f26062bd6dbe44
TUAN12K <tuannqph20022@fpt.edu.vn>
2023-11-20 22:50:14 +0700

    THANH TOAN - API - STAFF

--- src/main/java/com/example/pro2111_dat_lich_san_bong/core/staff/service/impl/ThanhToanSanCaStaffServiceImpl.java
+++ src/main/java/com/example/pro2111_dat_lich_san_bong/core/staff/service/impl/ThanhToanSanCaStaffServiceImpl.java
@@ -3,6 +3,8 @@ package com.example.pro2111_dat_lich_san_bong.core.staff.service.impl;
 import com.example.pro2111_dat_lich_san_bong.core.staff.model.request.HoaDonThanhToanRequest;
 import com.example.pro2111_dat_lich_san_bong.core.staff.reponsitory.HoaDonSanCaStaffRepository;
 import com.example.pro2111_dat_lich_san_bong.core.staff.service.IThanhToanSanCaStaffService;
+import com.example.pro2111_dat_lich_san_bong.core.user.service.SYSParamUserService;
+import com.example.pro2111_dat_lich_san_bong.enumstatus.TrangThaiHoaDonSanCa;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,14 +14,23 @@ import java.util.List;
 public class ThanhToanSanCaStaffServiceImpl implements IThanhToanSanCaStaffService {
     @Autowired
     private HoaDonSanCaStaffRepository hoaDonSanCaStaffRepository;
+    @Autowired
+    private SYSParamUserService sysParamUserService;
 
     @Override
-    public List<HoaDonThanhToanRequest> getAllHoaDonSanCas(int trangThai) {
-        return hoaDonSanCaStaffRepository.findAllByTrangThai(trangThai);
+    public List<HoaDonThanhToanRequest> getAllHoaDonSanCas() {
+        List<HoaDonThanhToanRequest> listhoaDonThanhToanRequest = hoaDonSanCaStaffRepository.findAllByTrangThai(TrangThaiHoaDonSanCa.DA_CHECK_IN.ordinal(), TrangThaiHoaDonSanCa.CHUA_THANH_TOAN.ordinal());
+        for (HoaDonThanhToanRequest hoaDonThanhToanRequest : listhoaDonThanhToanRequest) {
+            Double tongTienCoc = hoaDonThanhToanRequest.getTongTienSanCa();
+            hoaDonThanhToanRequest.setTienCoc(sysParamUserService.getTienCoc(tongTienCoc));
+        }
+        return listhoaDonThanhToanRequest;
     }
 
     @Override
     public HoaDonThanhToanRequest getOneHoaDonThanhToan(String id) {
-        return hoaDonSanCaStaffRepository.findOneById(id);
+        HoaDonThanhToanRequest hoaDonThanhToanRequest = hoaDonSanCaStaffRepository.findOneById(id);
+        hoaDonThanhToanRequest.setTienCoc(sysParamUserService.getTienCoc(hoaDonThanhToanRequest.getTongTienSanCa()));
+        return hoaDonThanhToanRequest;
     }
 }
