34c74cc1a3209030f6513b8b9bc5bb7a503c21ed
Phạm Văn Thể <thepvph20110@fpt.edu.vn>
2023-11-29 21:33:53 +0700

    update sysparamConstant

--- src/main/java/com/example/pro2111_dat_lich_san_bong/infrastructure/constant/SYSParamCodeConstant.java
+++ src/main/java/com/example/pro2111_dat_lich_san_bong/infrastructure/constant/SYSParamCodeConstant.java
@@ -24,10 +24,15 @@ public class SYSParamCodeConstant {
     //code để lấy thời gian đổi lịch trước ngày đến sân (cột value) trong bảng sys_param
     public static String THOI_GIAN_DOI_LICH = "THOI_GIAN_DOI_LICH";
 
+    public static String THOI_GIAN_CHO_PHEP_DAT_LICH = "THOI_GIAN_CHO_PHEP_DAT_LICH";
+
     //chuyen thành danh sach
     public static List<Object> LIST_PARAM_CODES = Arrays.asList(
             PHAN_TRAM_GIA_TIEN_COC,
             SO_COMBO_DAT,
-            THOI_GIAN_HET_GD
+            THOI_GIAN_HET_GD,
+            THOI_GIAN_THONG_BAO,
+            THOI_GIAN_DOI_LICH,
+            THOI_GIAN_CHO_PHEP_DAT_LICH
     );
 }
