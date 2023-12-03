3dc65797729622c5aa318d325b08aef71c6ba958
MSI\Admin <101080058+sydvph19885@users.noreply.github.com>
2023-12-03 17:55:47 +0700

    update profile

---
+++ src/main/java/com/example/pro2111_dat_lich_san_bong/core/user/service/AccountUserService.java
@@ -0,0 +1,7 @@
+package com.example.pro2111_dat_lich_san_bong.core.user.service;
+
+import com.example.pro2111_dat_lich_san_bong.entity.Account;
+
+public interface AccountUserService {
+    Account findById(String idAccount);
+}
